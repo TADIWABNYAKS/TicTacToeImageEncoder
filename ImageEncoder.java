c6142c9e6a7b8068fb81c71d449d9ee3c47bd1a5
Tadiwanashe Nyakonda <77730364+TADIWABNYAKS@users.noreply.github.com>
2024-10-24 12:48:36 +0200

    Update ImageEncoder.java
    1 3x3 board  per pixel limited color range , now each pixel is represented by 3 boards with each board being allocated to either R , G or B.  9 bits per board, most significant being default of 0 due to RGB range being 8 bits , color accuracy can actually be increased from this due to another bit but that's a bit overkill , Decoder will simply ignore most significant bit\

--- ImageEncoder.java
+++ ImageEncoder.java
@@ -1,11 +1,7 @@
-/** 
- * Encodes images into tic tac toe boards where each pixel is a board , all boards should be saved into a file to allow decoding back into the original image
- * @author TADIWABNYAKS 
- */
-
-import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ForkJoinPool;
@@ -20,33 +16,29 @@ public class ImageEncoder extends RecursiveTask<List<String[][]>> {
     private int startY;
     private int endX;
     private int endY;
-    private int numBoardsWide;
-    private int numBoardsHigh;
 
-    public ImageEncoder(BufferedImage image, int startX, int startY, int endX, int endY, int numBoardsWide, int numBoardsHigh) {
+    public ImageEncoder(BufferedImage image, int startX, int startY, int endX, int endY) {
         this.image = image;
         this.startX = startX;
         this.startY = startY;
         this.endX = endX;
         this.endY = endY;
-        this.numBoardsWide = numBoardsWide;
-        this.numBoardsHigh = numBoardsHigh;
     }
 
     @Override
     protected List<String[][]> compute() {
-        if ((endX - startX <= BOARD_SIZE) && (endY - startY <= BOARD_SIZE)) {
-            // Process a single 3x3 board
-            return encodeBoard(startX, startY);
+        if ((endX - startX == 1) && (endY - startY == 1)) {
+            // Process a single pixel (now represented by 3 boards)
+            return encodePixelAsBoards(startX, startY);
         } else {
             // Split the task into smaller subtasks
             int midX = (startX + endX) / 2;
             int midY = (startY + endY) / 2;
 
-            ImageEncoder topLeft = new ImageEncoder(image, startX, startY, midX, midY, numBoardsWide, numBoardsHigh);
-            ImageEncoder topRight = new ImageEncoder(image, midX, startY, endX, midY, numBoardsWide, numBoardsHigh);
-            ImageEncoder bottomLeft = new ImageEncoder(image, startX, midY, midX, endY, numBoardsWide, numBoardsHigh);
-            ImageEncoder bottomRight = new ImageEncoder(image, midX, midY, endX, endY, numBoardsWide, numBoardsHigh);
+            ImageEncoder topLeft = new ImageEncoder(image, startX, startY, midX, midY);
+            ImageEncoder topRight = new ImageEncoder(image, midX, startY, endX, midY);
+            ImageEncoder bottomLeft = new ImageEncoder(image, startX, midY, midX, endY);
+            ImageEncoder bottomRight = new ImageEncoder(image, midX, midY, endX, endY);
 
             invokeAll(topLeft, topRight, bottomLeft, bottomRight);
 
@@ -60,55 +52,90 @@ public class ImageEncoder extends RecursiveTask<List<String[][]>> {
         }
     }
 
-    // Encode 3x3 board based on pixel values
-    private List<String[][]> encodeBoard(int startX, int startY) {
+    private List<String[][]> encodePixelAsBoards(int x, int y) {
         List<String[][]> boards = new ArrayList<>();
-        String[][] board = new String[BOARD_SIZE][BOARD_SIZE];
 
-        for (int y = 0; y < BOARD_SIZE; y++) {
-            for (int x = 0; x < BOARD_SIZE; x++) {
-                int globalX = startX + x;
-                int globalY = startY + y;
-                if (globalX < image.getWidth() && globalY < image.getHeight()) {
-                    int pixelValue = image.getRGB(globalX, globalY) & 0xFF;  // Grayscale value
-                    board[y][x] = (pixelValue > 128) ? "X" : "O";  // X for bright, O for dark
-                } else {
-                    board[y][x] = "O";  // Default to O
-                }
-            }
-        }
-        boards.add(board);
+        int rgb = image.getRGB(x, y);
+        int red = (rgb >> 16) & 0xFF;
+        int green = (rgb >> 8) & 0xFF;
+        int blue = rgb & 0xFF;
+
+        // Create three boards: one for Red, Green, and Blue
+        String[][] redBoard = encodeValueAsBoard(red);
+        String[][] greenBoard = encodeValueAsBoard(green);
+        String[][] blueBoard = encodeValueAsBoard(blue);
+
+        boards.add(redBoard);
+        boards.add(greenBoard);
+        boards.add(blueBoard);
+
         return boards;
     }
 
+    private String[][] encodeValueAsBoard(int value) {
+        String[][] board = new String[BOARD_SIZE][BOARD_SIZE];
+        String binaryString = String.format("%9s", Integer.toBinaryString(value)).replace(' ', '0');  // Ensure 9-bit binary
+
+        // Map the binary string to a 3x3 board, ignoring the most significant bit (assumed to be 0/O)
+        int index = 0;
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                if (index == 0) {
+                    board[i][j] = "O";  // Default most significant bit as 0/O
+                } else {
+                    board[i][j] = binaryString.charAt(index - 1) == '1' ? "X" : "O";
+                }
+                index++;
+            }
+        }
+        return board;
+    }
+
     // Encodes the image in parallel
-    public static List<String[][]> parallelEncode(BufferedImage image, int numBoardsWide, int numBoardsHigh) {
+    public static List<String[][]> parallelEncode(BufferedImage image) {
         ForkJoinPool pool = new ForkJoinPool();
-        ImageEncoder task = new ImageEncoder(image, 0, 0, image.getWidth(), image.getHeight(), numBoardsWide, numBoardsHigh);
+        ImageEncoder task = new ImageEncoder(image, 0, 0, image.getWidth(), image.getHeight());
         return pool.invoke(task); 
     }
 
     // Main method inside the class to handle encoding
     public static void main(String[] args) throws Exception {
 
+        BufferedImage image = ImageIO.read(new File("C://Users//tadiw//Desktop//TicTacToe//test1.png"));
 
-        BufferedImage image = ImageIO.read(new File("C://Users//tadiw//Desktop//TicTacToe//test1.jpg"));
-        int numBoardsWide = image.getWidth() / BOARD_SIZE;
-        int numBoardsHigh = image.getHeight() / BOARD_SIZE;
+        // Encode the image into Tic-Tac-Toe boards (3 boards per pixel)
+        List<String[][]> encodedBoards = ImageEncoder.parallelEncode(image);
 
-        // Encode the image into Tic-Tac-Toe boards 
-        List<String[][]> encodedBoards = ImageEncoder.parallelEncode(image, numBoardsWide, numBoardsHigh);
+        // Save boards into a file
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("C://Users//tadiw//Desktop//New folder//test.tac"))) {
+            for (int i = 0; i < encodedBoards.size(); i += 3) {
+                // Write Red board
+                writeBoard(bw, encodedBoards.get(i));
+                bw.write("R;");
 
-        //REPLACE 
-        // Print out the boards , REPLACE with saving boards into either .csv or .txt file for the decoding. 
-        for (String[][] board : encodedBoards) {
-            for (String[] row : board) {
-                System.out.println(String.join(" ", row));
+                // Write Green board
+                writeBoard(bw, encodedBoards.get(i + 1));
+                bw.write("G;");
+
+                // Write Blue board
+                writeBoard(bw, encodedBoards.get(i + 2));
+                bw.write("B;");
+                bw.newLine();
             }
-            System.out.println("---");
+        } catch (Exception e) {
+            System.out.println(e);
         }
-        //END REPLACE
 
-        System.out.println("Image successfully Tic Tac Toed (Encoded) ");
+        System.out.println("Image successfully Tic Tac Toed (Encoded)");
+    }
+
+    // Helper method to write a board to the file
+    private static void writeBoard(BufferedWriter bw, String[][] board) throws Exception {
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            bw.write(String.join(",", board[i]));
+            if (i < BOARD_SIZE - 1) {
+                bw.write(";");
+            }
+        }
     }
 }
